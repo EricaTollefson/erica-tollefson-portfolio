db3bc0f67a933ceaf9774544cda6cccab95858e7
Erica Tollefson <etollefson@cnm.edu>
2019-07-17 15:13:10 -0600

    revised milestone-1.php to include purpose, audience and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,6 +6,16 @@
 
 	</head>
 	<body>
+<h1>PWP Project: Portfolio Site for Erica Tollefson</h1>
+<h2>Purpose</h2>
+		<p>This project is a professional portfolio website to present examples of web development projects</p>
+<h2>Audience</h2>
+		<p>This site is designed to present work to potential web development clients. The site will be targeted to individuals and small business owners looking to hire a web developer to build a new website or update a current website. A secondary audience would include colleagues who would like to see recent examples of my work.</p>
+<h2>Goal</h2>
+		<p> The primary goal of this site is to have a location where potential clients will be able to easily and quickly view my work, understand my background and contact me.</p>
+<h2>Persona</h2>
+		<img src="golden-circle.jpg" alt="golden circle diagram" height="549" width="615">
+
 
 	</body>
 </html>
