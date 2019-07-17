48aa2706d7cc28f5561ab33aa7754ef0fcb7035d
Erica Tollefson <etollefson@cnm.edu>
2019-07-17 15:53:59 -0600

    revised milestone-1.php to include persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,14 +8,23 @@
 	<body>
 <h1>PWP Project: Portfolio Site for Erica Tollefson</h1>
 <h2>Purpose</h2>
-		<p>This project is a professional portfolio website to present examples of web development projects</p>
+		<p>This project is a professional portfolio website to present examples of web development projects to potential clients.</p>
 <h2>Audience</h2>
 		<p>This site is designed to present work to potential web development clients. The site will be targeted to individuals and small business owners looking to hire a web developer to build a new website or update a current website. A secondary audience would include colleagues who would like to see recent examples of my work.</p>
 <h2>Goal</h2>
 		<p> The primary goal of this site is to have a location where potential clients will be able to easily and quickly view my work, understand my background and contact me.</p>
+		<br>
 <h2>Persona</h2>
-		<img src="golden-circle.jpg" alt="golden circle diagram" height="549" width="615">
+		<img src="self-portrait.jpg" alt="artist self-portrait" height="350" width="429">
 
 
+		<p><b>Name:</b> Edna Artist</p>
+		<p><b>Age:</b> 55</p>
+		<p><b>Profession:</b> Lifelong self-employed painter who is able support herself primarily through her art, with some additional financial resources through family.</p>
+		<p><b>Technology:</b> Edna has a basic comfort level with technology. She uses an older smartphone, a Macbook Air, and uses email and Facebook daily to communicate with friends, family and clients. She has a portfolio website that she uses so that clients can have access to her work and contact information, but the site has not been updated in 4 years, is very out of date, and does not include her recent work.</p>
+		<p><b>Attitudes and Behaviors:</b> Edna would prefer not to have to deal with technology, but understands those around her (like clients) believe it is important.</p>
+		<p><b>Needs:</b> Edna would like to have a modern, minimal website that she will not have to think about very often, and will have someone else to update as needed. She doesn't want to spend a lot of time on refreshing a website, and would like to know it will be done soon. Edna was frustrated with the experience building her website previously and would like to have confidence that whoever works on her site next will listen to her needs and communicate clearly regarding the process.</p>
+		<p><b>Goals:</b> Edna wants to be able to quickly and easily find a web developer for her site that has relevant experience, good design sense, and is easy to communicate with. </p>
+		<p><b>User Story:</b> As a client, I want to easily find a web developer to refresh my current site for my business.</p>
 	</body>
 </html>
