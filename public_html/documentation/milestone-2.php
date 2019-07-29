dce5963d300a08c4cbec3c9d70557bce6ada7ae9
Erica Tollefson <etollefson@cnm.edu>
2019-07-28 19:57:22 -0600

    initial commit of milestone-2.php without wireframes

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone 2: Erica Tollefson</title>
+
+	</head>
+	<body>
+		<h1>PWP Project: Milestone 2a</h1>
+		<h2>Content Strategy</h2>
+		<p>This project is a professional portfolio website to present examples of web development projects to potential clients. Each section will lay on a black background with a slight graphic treatment, and the text will fade onto the screen when the page is first loaded.</p>
+		<h3>Header</h3>
+		<p>The header will include an abstract photo that fades into the background, next to my name, title, and links to the other sections of the page,</p>
+		<h3>About</h3>
+		<p>The about section will include a brief description of my background, employment goals, and technical skills and technology expertise.</p>
+		<h3>Portfolio</h3>
+		<p>The portfolio section will feature a technical description of my capstone project, along with a picture of the site and a link to the site.</p>
+		<h3>Contact</h3>
+		<p>The contact section will feature a simple contact form, next to links to Github, linkedin, and my resume.</p>
+		<h3>Footer</h3>
+		<p>The footer will include nav links, along with icon links to Github, linkedin, and my resume.</p>
+		<br>
+		<h2>Wireframes</h2>
+		<img src="self-portrait.jpg" alt="artist self-portrait" height="350" width="429">
+
+	</body>
+</html>
