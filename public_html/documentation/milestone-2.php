51881da9212781fc275d7837c312eff08c08745b
Erica Tollefson <etollefson@cnm.edu>
2019-07-31 13:54:39 -0600

    commit milestone-2 with wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,25 +3,29 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>PWP Milestone 2: Erica Tollefson</title>
-
 	</head>
 	<body>
 		<h1>PWP Project: Milestone 2a</h1>
 		<h2>Content Strategy</h2>
-		<p>This project is a professional portfolio website to present examples of web development projects to potential clients. Each section will lay on a black background with a slight graphic treatment, and the text will fade onto the screen when the page is first loaded.</p>
+		<p>This project is a single page professional portfolio website to present examples of web development projects to potential clients. Each 		section will lay on a dark background with a slight graphic treatment (represented vaguely by a scribble line in the wireframes), and the text 		will fade onto the screen when each section is first loaded.</p>
 		<h3>Header</h3>
-		<p>The header will include an abstract photo that fades into the background, next to my name, title, and links to the other sections of the page,</p>
+		<p>The header will include an abstract photo that fades into the background, next to my name, title, and links to the other sections of the 		page.</p>
 		<h3>About</h3>
 		<p>The about section will include a brief description of my background, employment goals, and technical skills and technology expertise.</p>
 		<h3>Portfolio</h3>
-		<p>The portfolio section will feature a technical description of my capstone project, along with a picture of the site and a link to the site.</p>
+		<p>The portfolio section will feature a technical description of my capstone project, along with a picture of the site and a link to the 		site.</p>
 		<h3>Contact</h3>
-		<p>The contact section will feature a simple contact form, next to links to Github, linkedin, and my resume.</p>
+		<p>The contact section will feature a simple contact form, next to links to Github, Linkedin, and my resume.</p>
 		<h3>Footer</h3>
-		<p>The footer will include nav links, along with icon links to Github, linkedin, and my resume.</p>
+		<p>The footer will include nav links (or a hamburger menu on mobile), along with icon links to Github and Linkedin.</p>
 		<br>
 		<h2>Wireframes</h2>
-		<img src="self-portrait.jpg" alt="artist self-portrait" height="350" width="429">
-
+		<h3>Mobile</h3>
+			<img src="PWP-mobile-header.jpg" alt="mobile wireframe of website" height="733" width="362">
+			<img src="PWP-mobile-about.jpg" alt="mobile wireframe of website" height="730" width="368">
+			<img src="PWP-mobile-portfolio.jpg" alt="mobile wireframe of website" height="732" width="373">
+			<img src="PWP-mobile-contact.jpg" alt="mobile wireframe of website" height="731" width="370">
+		<h3>Desktop</h3>
+			<img src="PWP-desktop.jpg" alt="desktop wireframe of website" height="1428" width="840">
 	</body>
 </html>
