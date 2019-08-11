ea649100ad7ad97d160f6cf3fc6e2c24c2dda487
Erica Tollefson <etollefson@cnm.edu>
2019-08-11 12:38:07 -0600

    add footer navbar with basic structure

--- public_html/index.php
+++ public_html/index.php
@@ -93,5 +93,24 @@
 			</div>
 		</div>
 
+		<div class="clearfix">
+			<nav class="navbar navbar-collapse-xs navbar-expand-md navbar-light" style="background-color: #ebebeb;">
+				<div class="container d-inline ml-0">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+							  aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+						<div class="navbar-nav ml-auto">
+							<a class="nav-item nav-link" href="#">Home</a>
+							<a class="nav-item nav-link" href="#">About</a>
+							<a class="nav-item nav-link" href="#">Portfolio</a>
+							<a class="nav-item nav-link" href="#">Contact</a>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+
 	</body>
 </html>
